d077a854901fba3d1ae5b26306abeaeb7bfcb53a
Wolfxela <alexthegreitsas@gmail.com>
2023-09-22 05:43:27 +0300

    added gameboard and scoreboard, made it so the game is playable in console

--- script.js
+++ script.js
@@ -18,14 +18,15 @@ const playermaker = function(inputPlayerName)
 }
 
 
-const gameBoard = (function()
+
+const scoreBoard = (function()
 {
     const players = [];
-    let goal = 3;
+    let goal = 1;
     let player1Score = 0;
     let player2Score = 0;
 
-   const addPlayer = function(player)
+    const addPlayer = function(player)
     {
         if(players.length < 2)
         {
@@ -39,7 +40,7 @@ const gameBoard = (function()
             players[1].setSign("O")
         }
     }
-   const setPlayerScore = function(sign)
+    const setPlayerScore = function(sign)
     {
         if(players[0].getSign() == sign)
         {
@@ -75,10 +76,69 @@ const gameBoard = (function()
 
 })();
 
-//testing
-gameBoard.addPlayer(playermaker("Bob"))
-gameBoard.addPlayer(playermaker("Bil"))
+const gameBoard = (function()
+{
+    let tempNum = 0;
+    const board = ["","","","","","","","",""];
+    //the arrays are -1 because of index, first one would be 1 2 3 but is instead 0 1 2
+    const winningLines = [
+        [0,1,2],
+        [0,3,6],
+        [0,4,8],
+        [2,5,8],
+        [3,4,5],
+        [4,1,7],
+        [6,4,2],
+        [6,7,8]
+    ]
 
-gameBoard.setPlayerScore("X")
-gameBoard.setPlayerScore("X")
-gameBoard.setPlayerScore("X")
+    const placeMark = function(Spot)
+    {
+        placeSpot = Spot -1
+    
+        if(tempNum == 0 && board[placeSpot] == "")
+        {
+            board[placeSpot] = "X"
+            tempNum += 1
+            if(checkBoard() == true)
+            {
+                scoreBoard.setPlayerScore("X")
+            }
+        }
+        else if(tempNum != 0 && board[placeSpot] == "")
+        {
+            board[placeSpot] = "O"
+            tempNum = 0
+            if(checkBoard() == true)
+            {
+                scoreBoard.setPlayerScore("O")
+            }
+        }
+
+       
+    }
+    const checkBoard = function()
+    {
+
+        for(let i = 0; i < winningLines.length; i++)
+        {
+            if(board[winningLines[i][0]] != "" && board[winningLines[i][0]] == board[winningLines[i][1]] && board[winningLines[i][0]] == board[winningLines[i][2]])
+        {
+            return true
+        }
+        }
+    }
+
+    return{placeMark}
+
+})();
+scoreBoard.addPlayer(playermaker("bob"))
+scoreBoard.addPlayer(playermaker("bill"))
+//testing
+gameBoard.placeMark(9)
+gameBoard.placeMark(1)
+gameBoard.placeMark(8)
+gameBoard.placeMark(2)
+gameBoard.placeMark(6)
+gameBoard.placeMark(3)
+
