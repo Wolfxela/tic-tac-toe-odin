e3228b3cb2e930a1103bdd62c4e27d96de71460c
Wolfxela <alexthegreitsas@gmail.com>
2023-09-26 04:39:05 +0300

    finished the project

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const playermaker = function(inputPlayerName)
 
    return{getSign,getPlayerName,setSign}
 }
+//ai seems to bug out when the player chooses to cancel the ai's winning power, to fix
 
 
 const ai = function(inputrobotName)
@@ -33,25 +34,44 @@ const ai = function(inputrobotName)
         for(let i = 0; i < tempBoard.length;i++)
         {
             
-            let tempBoard = [...currentBoard]
+            tempBoard = [...currentBoard]
               //if it finds out that he can win, he will place it on that spot
             if(tempBoard[i] != "X" && tempBoard[i] != "O")
             {
                 tempBoard[i] = "O"
+        
             }
             if(gameBoard.checkBoard(tempBoard) == true)
             {
+                console.log(i)
                 return i + 1
+
             }
-            //if it finds that the player can win if he chooses a spot, it will place it in that spot if he can't win this turn
-            if(tempBoard[i] != "X" && tempBoard[i] != "O")
+            else
             {
-                tempBoard[i] = "X"
+                tempBoard[i] = ""
             }
-            if(gameBoard.checkBoard(tempBoard) == true)
-            {
-                return i + 1
-            }
+         
+
+        }
+        for(let i = 0; i < tempBoard.length;i++)
+        {
+            tempBoard = [...currentBoard]
+               //if it finds that the player can win if he chooses a spot, it will place it in that spot if he can't win this turn
+               if(tempBoard[i] != "X" && tempBoard[i] != "O")
+               {
+                   tempBoard[i] = "X"
+   
+               }
+               if(gameBoard.checkBoard(tempBoard) == true)
+               {
+                   return i + 1
+                   
+               }
+               else
+                {
+                    tempBoard[i] = ""
+                }
         }
         //if he can't win and there's no threat of losing, randomly choose an empty spot(possibly change later to make it smarter and have strategies)
         for(let i = 0; i < tempBoard.length;i++)
@@ -59,6 +79,7 @@ const ai = function(inputrobotName)
             answer = Math.floor(Math.random() * 9)+1
             if(currentBoard[answer -1] != "X" && currentBoard[answer -1] != "O")
             {
+                console.log("hello")
                 return answer
             }
         }
@@ -69,13 +90,18 @@ const ai = function(inputrobotName)
 }
 
 
-
 const scoreBoard = (function()
 {
     const players = [];
-    let goal = 1;
+    let goal = 3;
     let player1Score = 0;
     let player2Score = 0;
+
+    const gameBody = document.querySelector('.gameBody');
+    const playerScoreBox = gameBody.querySelector('.playerScore');
+    const aiScoreBox = gameBody.querySelector('.aiScore');
+    const playerNameBox = gameBody.querySelector('.playerName');
+    
 
     const addPlayer = function(player)
     {
@@ -83,6 +109,7 @@ const scoreBoard = (function()
         {
             players.push(player)
             players[0].setSign("X")
+            playerNameBox.textContent = players[0].getPlayerName();
             
         }
         else if(players.length >= 2)
@@ -96,10 +123,12 @@ const scoreBoard = (function()
         if(players[0].getSign() == sign)
         {
             player1Score += 1
+            playerScoreBox.textContent = player1Score;
         }
         else
         {
             player2Score += 1
+            aiScoreBox.textContent = player2Score;
         }
         checkWinner()
 
@@ -131,11 +160,15 @@ const scoreBoard = (function()
 
 })();
 
+
 const gameBoard = (function()
 {
     let tempNum = 0;
     let turns = 0;
     const board = ["","","","","","","","",""];
+    const gameBody = document.querySelector('.gameBody');
+    const boxes = gameBody.querySelectorAll('.box');
+    
     //the arrays are -1 because of index, first one would be 1 2 3 but is instead 0 1 2
     const winningLines = [
         [0,1,2],
@@ -145,7 +178,7 @@ const gameBoard = (function()
         [3,4,5],
         [4,1,7],
         [6,4,2],
-        [6,7,8]
+        [6,7,8],
     ]
     
     const placeMark = function(Spot)
@@ -157,12 +190,17 @@ const gameBoard = (function()
         {
 
             board[placeSpot] = "X"
+            boxes[placeSpot].textContent = "X"
             tempNum += 1
             turns += 1;
             
             if(checkBoard(board) == true)
             {
                 scoreBoard.setPlayerScore("X")
+                clearBoard(board,false)
+                clearBoard(boxes,true)
+                tempNum = 0;
+                turns = 0
                
                 
             }
@@ -175,18 +213,27 @@ const gameBoard = (function()
         else if(tempNum != 0 && board[placeSpot] == "")
         {
             board[placeSpot] = "O"
+            boxes[placeSpot].textContent = "O"
             tempNum = 0
             turns += 1;
             if(checkBoard(board) == true)
             {
                 scoreBoard.setPlayerScore("O")
+                clearBoard(board,false)
+                clearBoard(boxes,true)
+                tempNum = 0;
+                turns = 0
                
             }
         }
 
-        if(turns == 10)
+
+        if(turns == 9)
         {
-            clearBoard();
+            tempNum = 0
+            turns = 0
+            clearBoard(board,false)
+            clearBoard(boxes,true)
         }
         
 
@@ -194,7 +241,7 @@ const gameBoard = (function()
     }
     const checkBoard = function(inputBoard)
     {
-
+        
         for(let i = 0; i < winningLines.length; i++)
         {
             if(inputBoard[winningLines[i][0]] != ""
@@ -204,27 +251,43 @@ const gameBoard = (function()
                inputBoard[winningLines[i][0]] == inputBoard[winningLines[i][2]]
                )
             {
-                clearBoard(inputBoard);
+                clearBoard(inputBoard,false);
                 return true
             }
         }
     }
-    const clearBoard = function(inputBoard)
+    const clearBoard = function(inputBoard,isNodeList)
     {
-
-        for(let i = 0; i < inputBoard.length;i++)
+        if(isNodeList === false)
         {
-            inputBoard[i] = "";
+            for(let i = 0; i < inputBoard.length;i++)
+            {
+                inputBoard[i] = "";
+            }
         }
+        else
+        {
+            for(let i = 0; i < inputBoard.length;i++)
+            {
+                inputBoard[i].textContent = "";
+            }
+        }
+      
 
     }
+    const setButtons = function()
+    {
+        for(let i = 0; i < boxes.length;i++)
+        {
+                boxes[i].addEventListener("click",function(){placeMark(i+1)})
+        }
+    }
 
-    return{placeMark,checkBoard}
+
+    return{placeMark,checkBoard,setButtons}
 
 })();
-scoreBoard.addPlayer(playermaker("bob"))
+gameBoard.setButtons()
+scoreBoard.addPlayer(playermaker("Player"))
 scoreBoard.addPlayer(ai("bill"))
 //testing
-gameBoard.placeMark(6)
-
-
